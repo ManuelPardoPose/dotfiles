41baa24ed7797d093e8537ed1becb2bc5e0459c8
ManuelPardoPose <76797617+ManuelPardoPose@users.noreply.github.com>
2024-02-25 11:33:17 +0100

    Added my nvim config
    some custom styling, catppuccin theme, file tree plugin, markdown render plugin

---
+++ nvim/init.lua
@@ -0,0 +1,54 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+
+require("lazy").setup("plugins", opts)
+
+vim.cmd.colorscheme "catppuccin"
+
+-- line numbers
+vim.opt.nu = true
+vim.opt.rnu = true
+
+-- tabs
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+-- auto indent
+vim.opt.smartindent = true
+vim.opt.shiftwidth = 4
+
+-- smart indent
+vim.opt.breakindent = true
+
+-- disable text wrap
+vim.opt.wrap = false
+
+vim.opt.signcolumn = "yes"
+
+-- system clipboard
+vim.opt.clipboard = "unnamed,unnamedplus"
+
+-- enables highlighting of cursor line
+vim.opt.cursorline = true
+
+-- restrict scrolloff spacing
+vim.opt.scrolloff = 10
+
+vim.opt.colorcolumn = "80"
+
+
+-- KEYMAP
+vim.keymap.set('n', 't', '<cmd>NvimTreeToggle<cr>')
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,16 @@
+return {
+	-- catppuccin theme
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	
+    -- autopairs for brackets, braces etc.
+    { "windwp/nvim-autopairs", event = "InsertEnter", opts = {} },
+	
+    -- file tree/explorer
+    { "nvim-tree/nvim-tree.lua", opts = { on_attach = on_attach_change } },
+    
+    -- markdown preview
+    { "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end, }
+}
