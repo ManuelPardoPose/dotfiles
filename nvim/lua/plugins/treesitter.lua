15b93c8c068f0a82a528479fefaac19c06a2c21d
Manuel Pardo Pose <mpardopose@gmail.com>
2026-05-20 02:54:23 +0200

    fix treesitter bug

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,10 @@
 -- Treesitter
 return {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter.configs").setup({
+        require("nvim-treesitter.config").setup({
             -- A list of parser names, or "all"
             ensure_installed = {
                 "vimdoc", "javascript", "typescript", "c", "lua", "rust",
